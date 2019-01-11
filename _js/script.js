bb9d414874c5ec833a816b3d324a98f47b66f960
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2019-01-11 15:15:36 -0200

    A logica criada no commit anterior dificultou muito o projeto e ela foi refeita.

--- _js/script.js
+++ _js/script.js
@@ -7,50 +7,57 @@ $(document).ready(function() {
     var totalDeMinas = 30;
     var celulasComMinas = [];
     
+    
+    //Sorteia os campos que terão bombas
     for (var x = 0; x < 30; x++) {
         var campoSorteado = parseInt(Math.random() * 100);
-
+        
         while (celulasComMinas.indexOf(campoSorteado) != -1) {
             campoSorteado = parseInt(Math.random() * 100);
         }
-        
         celulasComMinas.push(campoSorteado);
-        
-        objCampos = {
-            indice: campoSorteado
-        };
-        
-        arrayCampos.push(objCampos);
+
     }
     
+    //Cria os campos
+    for (var x = 0; x < 100; x++) {
+        if (celulasComMinas.indexOf(x) == -1) {
+            objCampos = {
+                indice: x,
+                temBomba: false
+            };
+        } else {
+            objCampos = {
+                indice: x,
+                temBomba: true
+            };
+        }
+        arrayCampos.push(objCampos);
+        objCampos = {};
+    }
     
-    
-    
+    //Preenche os campos
+//    for (var x = 0; x < 10; x++) {
+//        html += "<tr>";
+//        for (var y = 0; y < 10; y++) {
+//            if (arrayCampos[celula].temBomba == true) {
+//                html += "<td><span style = 'color: red'>X</span></td>";
+//            } else {
+//                html += "<td>" + arrayCampos[celula].indice + "</td>";
+//            }
+//            celula++;
+//        }
+//        html+= "</tr>";
+//    }
+
     for (var x = 0; x < 10; x++) {
         html += "<tr>";
         for (var y = 0; y < 10; y++) {
-            if (celulasComMinas.indexOf(celula) != -1) {
-//                arrayCampos[celula] = {
-//                    temBomba: true
-//                }
-                
-//                objCampos = {
-//                    temBomba: true
-//                };
-                html += "<td><span style = 'color: red'>X</span></td>";
-            } else {
-//                arrayCampos[celula] = {
-//                    temBomba: false
-//                }
-//                objCampos = {
-//                    temBomba: false
-//                };
-                html += "<td>" + celula + "</td>";
-            }
-//            
-            celula++;
+            html += "<td></td>";
         }
+        html += "</tr>";
     }
-//    console.log(arrayCampos);
+    
     $("#tblCampo").html(html);
+    
 });
